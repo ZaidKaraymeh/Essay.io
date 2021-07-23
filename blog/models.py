1528fd62c2406c44f35557f0e8192b0d2b6877b9
Zaid <karaymehzaid@gmail.com>
2021-07-23 16:23:26 +0300

    Home Screen Updated

--- blog/models.py
+++ blog/models.py
@@ -11,5 +11,5 @@ class Essay(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse("blog-paper", kwargs={"id":str(self.id)})
+    # def get_absolute_url(self):
+    #     return reverse("blog-paper", kwargs={"id":str(self.id)})
